6b1f0858220fef9df30ee32d921419e999de15e2
Ricardo Tlatelpa <rtlatlepa4@gmail.com>
2019-11-21 08:30:34 -0500

    Updates

---
+++ precache-manifest.fd632629c9fbd93a562e8a79acb7a0fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "affe223e87252cb8f07d9da5eff09a1c",
+    "url": "/finallyacalculator/index.html"
+  },
+  {
+    "revision": "b621d15bca406395d96b",
+    "url": "/finallyacalculator/static/css/main.ae79978e.chunk.css"
+  },
+  {
+    "revision": "c5ab976b356081467e2f",
+    "url": "/finallyacalculator/static/js/2.8b2ac56b.chunk.js"
+  },
+  {
+    "revision": "b621d15bca406395d96b",
+    "url": "/finallyacalculator/static/js/main.69f5101c.chunk.js"
+  },
+  {
+    "revision": "3ce1ee8213a0174d9297",
+    "url": "/finallyacalculator/static/js/runtime-main.5490db25.js"
+  }
+]);
